acb90ea1a412ab620d2a5a91723b17fd8b1b163c
pabloalmunia <pablo.almunia@corusconsulting.com>
2023-11-14 08:39:38 +0100

    fix(tools-workbench): source formatting error when repeating the same tag

--- tools/workbench/static/source-format.js
+++ tools/workbench/static/source-format.js
@@ -5,22 +5,24 @@ function sourceFormat (source) {
   transformed     = transformed.replace(/>\s</g, '><');
   let n           = 0;
   let i           = 0;
-  let command     = '';
+  let pre         = '';
+  let ant         = '';
   while (n < transformed.length) {
     if (transformed[n] === '<') {
       const current = transformed.substring(n).match(/\w+/g)[0];
       if (transformed[n + 1] === '/') {
         i -= 2;
-        if (current !== command) {
+        if (current !== pre || pre === ant) {
           transformed = transformed.slice(0, n) + '\n' + ' '.repeat(i) + transformed.slice(n);
           n += i + 1
         }
       } else {
+        ant         = pre;
         transformed = transformed.slice(0, n) + '\n' + ' '.repeat(i) + transformed.slice(n);
         n += i + 1
         i += 2;
       }
-      command = current;
+      pre = current;
     }
     n++;
   }
