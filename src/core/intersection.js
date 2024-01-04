8675fffc55d8ef4f5f2b5520878261450b806ecf
pabloalmunia <pablo.almunia@corusconsulting.com>
2024-01-04 09:52:46 +0100

    feat(intersection): improve intersection event handling
    Implemented an observer function within the intersection script to prevent multiple event calls and added event resend functionality. This change ensures that any intersection enter and exit events are properly dispatched even if the element has not yet rendered.

--- src/core/intersection.js
+++ src/core/intersection.js
@@ -2,6 +2,8 @@ import { CONTEXT, FIRE_EVENT } from "./base.js";
 import { STRING, NUMBER }      from '../helpers/types.js';
 import intersection            from "../helpers/intersection.js";
 
+const OBSERVER = Symbol();
+
 export function intersectionCoreExtension (defineObject) {
   defineObject
     .attribute({name : 'intersection-once-class', type : STRING, value : ''})
@@ -11,16 +13,34 @@ export function intersectionCoreExtension (defineObject) {
 
 const add    = (classList, classes) => classes && classes.split(/\s+/).forEach(cl => classList.add(cl));
 const remove = (classList, classes) => classes && classes.split(/\s+/).forEach(cl => classList.remove(cl));
+const resend = (el, event) => {
+  event.preventDefault();
+  const resend = () => {
+    el.removeEventListener('render', resend);
+    el[FIRE_EVENT](event.type, event.detail, event.composed);
+  }
+  el.addEventListener('render', resend)
+}
 
 function observer () {
+  if (this[OBSERVER]) {
+    return;
+  }
+  this[OBSERVER] = true;
   const ctx       = this[CONTEXT];
   const classList = this.classList;
-  this.addEventListener('intersection.enter', () => {
+  this.addEventListener('intersection.enter', (event) => {
     add(classList, ctx.intersectionOnceClass);
     add(classList, ctx.intersectionClass);
+    if (!this.rendered) {
+      resend(this, event);
+    }
   });
-  this.addEventListener('intersection.exit', () => {
+  this.addEventListener('intersection.exit', (event) => {
     remove(classList, ctx.intersectionClass);
+    if (!this.rendered) {
+      resend(this, event);
+    }
   });
   intersection.call(this, ctx.intersectionRatio);
 }
