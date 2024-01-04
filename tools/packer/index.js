6313c776808fc314f49bc5005eca7ed871c501c9
pabloalmunia <pablo.almunia@corusconsulting.com>
2024-01-04 09:51:46 +0100

    feat(tools): add map option to packer tool
    The packer tool has been updated to include a new command line option, '-x/--map', to allow the option to build a .map file. Adjustments have been also made in the pack.js file to incorporate this option into the bundling process. If the option is not invoked, the tool follows the usual procedure of versioning, optimizing, and processing the files.

--- tools/packer/index.js
+++ tools/packer/index.js
@@ -6,6 +6,7 @@ program
   .command ('build <from> <to> [parckage.json]', {isDefault : true})
   .option ('-m, --module', 'build a ES module')
   .option ('-e, --exclude <module...>', 'exclude module from the bundle')
+  .option ('-x, --map', 'build .map file')
   .action (pack);
 program.parse(process.argv);
 
--- tools/packer/pack.js
+++ tools/packer/pack.js
@@ -5,6 +5,8 @@ const optimizeTemplateString = require('./optimizeTemplateString.js');
 
 async function pack (from, to, packagejson, options) {
 
+  console.log(options);
+
   //-------------------------------------------------------
   // Original size
   //-------------------------------------------------------
@@ -27,7 +29,7 @@ async function pack (from, to, packagejson, options) {
     bundle      : true,
     format      : options.module ? 'esm' : 'iife',
     external    : options.exclude ? options.exclude : undefined,
-    sourcemap   : 'linked',
+    sourcemap   : options.map ? 'linked' : '',
     drop        : ['debugger']
   });
 
@@ -37,48 +39,53 @@ async function pack (from, to, packagejson, options) {
   let content = (await fs.readFile(to)).toString();
   !options.silence && console.log('           size after minimize:', content.length);
 
-  //-------------------------------------------------------
-  // Add name and version
-  //-------------------------------------------------------
-  packagejson = packagejson || './package.json';
-  const pck = require(path.resolve(process.cwd(), packagejson));
-  content   = `/* ${ pck.name }${ pck.version ? ` - ${ pck.version }` : '' } */ ` + content;
-  !options.silence && console.log('          size after commented:', content.length);
+  if (!options.map) {
 
-  //-------------------------------------------------------
-  // Processing file
-  //-------------------------------------------------------
+    //-------------------------------------------------------
+    // Add name and version
+    //-------------------------------------------------------
+    packagejson = packagejson || './package.json';
+    const pck   = require(path.resolve(process.cwd(), packagejson));
+    content     = `/* ${ pck.name }${ pck.version ? ` - ${ pck.version }` : '' } */ ` + content;
+    !options.silence && console.log('          size after commented:', content.length);
 
-  // Optimize template string
-  content = optimizeTemplateString(content);
-  !options.silence && console.log('after template string optimize:', content.length);
+    //-------------------------------------------------------
+    // Processing file
+    //-------------------------------------------------------
 
-  // Optimize const to let
-  content = content.replace(/const /g, 'let ');
-  !options.silence && console.log('  after replace const with let:', content.length);
+    // Optimize template string
+    content = optimizeTemplateString(content);
+    !options.silence && console.log('after template string optimize:', content.length);
 
-  // Remove lines
-  content = content.replace(/\n/g, ' ');
-  !options.silence && console.log('         after end line remove:', content.length);
+    // Optimize const to let
+    content = content.replace(/const /g, 'let ');
+    !options.silence && console.log('  after replace const with let:', content.length);
 
-  // Remove IIFF argument
-  content = content.replace ('.call(this || window, (window.__wpcc = window.__wpcc || {}))', '()');
-  content = content.replace ('function(__wpcc)', '() => ');
-  console.log ('    after IIFF argument remove:', content.length);
+    // Remove lines
+    content = content.replace(/\n/g, ' ');
+    !options.silence && console.log('         after end line remove:', content.length);
 
-  // Remove spaces
-  content = content.replace(/\s{2,}/g, ' ');
-  content = content.trim();
-  !options.silence && console.log('            after space remove:', content.length);
+    // Remove IIFF argument
+    content = content.replace('.call(this || window, (window.__wpcc = window.__wpcc || {}))', '()');
+    content = content.replace('function(__wpcc)', '() => ');
+    console.log('    after IIFF argument remove:', content.length);
+
+    // Remove spaces
+    content = content.replace(/\s{2,}/g, ' ');
+    content = content.trim();
+    !options.silence && console.log('            after space remove:', content.length);
+
+    //-------------------------------------------------------
+    // Write to file
+    //-------------------------------------------------------
+    await fs.writeFile(to, content);
+
+  }
 
   // Compare size
   !options.silence && console.log('                    difference:', content.length - prev.length);
 
-
-  //-------------------------------------------------------
-  // Write to file
-  //-------------------------------------------------------
-  await fs.writeFile(to, content);
+  // Last message
   !options.silence && console.log(`
 file ${ to } written
 `);
