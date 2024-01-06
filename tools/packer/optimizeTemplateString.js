25334846d51c40f6560b63e04c7a6f850f9effc3
pabloalmunia <pablo.almunia@corusconsulting.com>
2024-01-06 12:06:01 +0100

    fix(tools): refactor packer tool to handle errors
    Refactored the 'optimizeTemplateString' function  to include 'esprima-next' error handling. The function now utilizes a try-catch block to deal with potential errors during function execution, enhancing the stability of the code.

--- tools/packer/optimizeTemplateString.js
+++ tools/packer/optimizeTemplateString.js
@@ -1,31 +1,30 @@
-const fs      = require ('fs');
-const esprima = require ('esprima-next');
-
-// const source = fs.readFileSync('../../Barchart/dist/barchart.js').toString();
-// console.log(optimizeTemplateString(source))
+const esprima = require('esprima-next');
 
 module.exports = function optimizeTemplateString (source) {
-// function optimizeTemplateString (source) {
-  //debugger;
-  const ast       = esprima.parseModule (source, {range : true});
-  const templates = findTemplateLiteral (ast);
-  const target    = buildSource (source, templates);
-  return target;
+  try {
+    debugger;
+    const ast       = esprima.parseModule(source, {range : true});
+    const templates = findTemplateLiteral(ast);
+    return buildSource(source, templates);
+  } catch (err) {
+    console.error(err);
+    return source;
+  }
 };
 
 function findTemplateLiteral (obj) {
   const result = [];
   (function search (obj) {
     if (obj.type === 'TemplateLiteral') {
-      result.push (obj.range);
+      result.push(obj.range);
     } else {
       for (let prop in obj) {
-        if (typeof obj[ prop ] === 'object' && obj[ prop ] !== null) {
-          search (obj[ prop ]);
+        if (typeof obj[prop] === 'object' && obj[prop] !== null) {
+          search(obj[prop]);
         }
       }
     }
-  }) (obj);
+  })(obj);
   return result;
 }
 
@@ -33,16 +32,16 @@ function buildSource (source, templates) {
   let idx    = 0;
   let target = '';
   for (let range of templates) {
-    target += source.substring (idx, range[ 0 ]);
-    idx                   = range[ 1 ];
-    let template          = source.substring (range[ 0 ], range[ 1 ]);
-    let templateOptimized = optimizeTemplateString (template);
+    target += source.substring(idx, range[0]);
+    idx                   = range[1];
+    let template          = source.substring(range[0], range[1]);
+    let templateOptimized = optimizeTemplateString(template);
     target += templateOptimized;
   }
-  target += source.substring (idx);
+  target += source.substring(idx);
   return target;
 }
 
 function optimizeTemplateString (template) {
-  return template.replace (/\s\s+/g, ' ').replace (/>\s</g, '><').trim ();
+  return template.replace(/\s\s+/g, ' ').replace(/>\s</g, '><').trim();
 }
