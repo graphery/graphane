ce26bd31a3c09f363738b4db276489d759d70f12
pabloalmunia <pablo.almunia@corusconsulting.com>
2024-08-26 09:17:17 +0200

    Add test case for "g-if with g-for" functionality
    This test case checks the conditional rendering of circles within an SVG element based on the `display` flag. It ensures that the elements correctly toggle visibility and update dynamically on button click.

---
+++ test/plugins/template.engine/cases/case43.js
@@ -0,0 +1,42 @@
+export const title       = '43) g-if with g-for';
+export const description = 'conditional loop';
+
+export function script () {
+  const svg   = gSVG(document.querySelector('#svg'));
+  let display = true;
+  svg.render({
+    display,
+    values  : [
+      {cx : 25, cy : 25, color : 'red'},
+      {cx : 50, cy : 50, color : 'green'},
+      {cx : 75, cy : 75, color : 'blue'},
+    ]
+  });
+  document.querySelector('#update').addEventListener('click', () => {
+    display = !display;
+    svg.render({
+      display,
+      values  : [
+        {cx : 25, cy : 25, color : 'red'},
+        {cx : 50, cy : 50, color : 'green'},
+        {cx : 75, cy : 75, color : 'blue'},
+      ]
+    });
+    document.querySelector('#result').innerHTML = sourceFormat(svg.source());
+  });
+  document.querySelector('#result').innerHTML = sourceFormat(svg.source());
+}
+
+export default `<svg viewBox="0 0 100 100" id="svg" style="width: 100px; height: 100px">
+  <g g-if="display" >
+    <defs g-for="value of values">
+      <circle :cx   = "value.cx" 
+              :cy   = "value.cy"
+              :fill = "console.log('fill') || value.color"
+              r     = "25">
+      </circle>
+    </defs>
+  </g>
+</svg>
+<button id="update">update</button>
+<pre id="result"></pre>`;
