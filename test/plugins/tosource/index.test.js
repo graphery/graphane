a0b84ab7a7bbf67ab00192942ab9ca0be5b0d974
pabloalmunia <pablo.almunia@corusconsulting.com>
2024-08-26 09:17:03 +0200

    Refactor toSource function to exclude reference comments
    Updated the `toSource` function to simplify the element serialization process and to remove reference comments. Replaced the previous removal logic for elements with 'data-type' attribute set to 'graphane' with a more direct approach.

--- test/plugins/tosource/index.test.js
+++ test/plugins/tosource/index.test.js
@@ -14,12 +14,11 @@ const FOLDER          = './test/plugins/tosource/cases/';
 const results = {
   case01 : `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0,0,100,100" width="100" height="100"> <line x1="10" y1="10" x2="90" y2="90" stroke="black" stroke-width="10"/> <line x1="10" y1="90" x2="90" y2="10" stroke="black" stroke-width="10"/> </svg>`,
   case02 : `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" id="svg" style="width: 100px; height: 100px"> <text x="0" y="50">hello world</text> </svg>`,
-  case03 : `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" id="svg" style="width: 100px; height: 100px"> <circle cx="25" cy="50" r="20" fill="violet" style="visibility: inherit;"/> <circle cx="75" cy="50" r="20" fill="orange" style="visibility: hidden;"/> </svg>`,
+  case03 : `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" id="svg" style="width: 100px; height: 100px"> <circle cx="25" cy="50" r="20" fill="violet"/> </svg>`,
   case04 : `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" id="svg" style="width: 100px; height: 100px"> <rect x="10" y="10" width="80" height="80" fill="red"/> </svg>`,
   case05 : `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" id="svg" style="width: 100px; height: 100px"> <rect style="cursor: pointer" x="10" y="10" width="80" height="80" fill="red"></rect> </svg>`,
   case06 : `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" id="svg" style="width: 100px; height: 100px"> <g> <circle r="25" cx="25" cy="25" fill="red"></circle> </g> <g> <circle r="25" cx="50" cy="50" fill="green"></circle> </g> <g> <circle r="25" cx="75" cy="75" fill="blue"></circle> </g> </svg>`,
   case07 : `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" id="svg" style="width: 100px; height: 100px"> <defs> <circle id="test" r="25"/> </defs> <g> <use href="#test" id="circle_a" x="25" y="25" fill="red"/> <text fill="white" x="22" y="31">a</text> </g> <g> <use href="#test" id="circle_b" x="50" y="50" fill="green"/> <text fill="white" x="47" y="56">b</text> </g> <g> <use href="#test" id="circle_c" x="75" y="75" fill="blue"/> <text fill="white" x="72" y="81">c</text> </g> </svg>`,
-  case08 : `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0,0,100,100" width="100" height="100"> <line x1="10" y1="10" x2="90" y2="90" stroke="black" stroke-width="10"/> </svg>`,
 }
 
 const dir = await opendir(FOLDER);
